c2f1ea84c85371193b2539e6cd1425c328574e20
haoyuh3 <1187296801@qq.com>
2026-01-23 10:02:54 -0500

    change: provide shape type in main function instead of rectangle

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,21 +2,22 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
 
         Renderer renderer = new Renderer(rectangle);
 
         renderer.draw();
 
-        Circle circle = new Circle(2);
+        Shape circle = new Circle(2);
         Renderer renderer2 = new Renderer(circle);
         renderer2.draw();
 
-        Square square = new Square(2);
+        Shape square = new Square(2);
         Renderer renderer3 = new Renderer(square);
         renderer3.draw();
     }
